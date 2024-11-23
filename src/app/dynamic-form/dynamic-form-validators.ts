caf5662c8b8338952005374da99c947aceae92b8
Michael Graute <michael.graute@visitrans.de>
2024-11-23 13:03:28 +0100

    added custom validators

---
+++ src/app/dynamic-form/dynamic-form-validators.ts
@@ -0,0 +1,50 @@
+import {AbstractControl, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
+
+export class DynamicFormValidators {
+
+  static minItems(minItems: number): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return (control.value?.length < minItems || control.value === null) ? {minItems: {param: minItems, value: control.value?.length || 0}} : null;
+    };
+  }
+
+  static maxItems(maxItems: number): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return (control.value?.length > maxItems) ? {maxItems: {param: maxItems, value: control.value?.length || 0}} : null;
+    };
+  }
+
+  static inArray(allowedValues: string[]): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return allowedValues.includes(control.value) ? null : {inArray: {param: allowedValues.join(', '), value: control.value, allowedValues: allowedValues}};
+    }
+  }
+
+  static minLength(minLength: number): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return (control.value?.length < minLength || control.value === null) ? {minLength: {param: minLength,value: control.value?.length || 0}} : null;
+    };
+  }
+
+  static maxLength(maxLength: number): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return (control.value?.length > maxLength) ? {maxLength: {param: maxLength, value: control.value?.length}} : null;
+    };
+  }
+
+  static required(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if(control.value && Array.isArray(control.value)) {
+        return (control.value.length === 0) ? {required: true} : null;
+      }
+      return (!control.value || control.value === 'null') ? {required: true} : null;
+    }
+  }
+
+  static email(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return Validators.email(control);
+    }
+  }
+
+}
--- src/app/dynamic-form/form-elements/abstract-input.component.ts
+++ src/app/dynamic-form/form-elements/abstract-input.component.ts
@@ -1,7 +1,8 @@
 import {Component, HostBinding, OnDestroy, OnInit} from "@angular/core";
-import {FormControl, FormGroup, ValidatorFn, Validators} from "@angular/forms";
+import {FormControl, FormGroup, ValidatorFn} from "@angular/forms";
 import {FormElement} from "../dynamic-form.types";
 import {DynamicFormElementInterface} from "../dynamic-form-element.interface";
+import {DynamicFormValidators} from "../dynamic-form-validators";
 
 @Component({template: ``})
 export abstract class AbstractInputComponent implements DynamicFormElementInterface, OnInit, OnDestroy {
@@ -19,21 +20,8 @@ export abstract class AbstractInputComponent implements DynamicFormElementInterf
 
     let validators:ValidatorFn[] = [];
     this.config?.validators?.forEach((validator: any) => {
-      switch (validator.name) {
-        case 'email':
-          validators.push(Validators.email)
-          break;
-        case 'required':
-          validators.push(Validators.required);
-          if(this.config) this.config.required = true
-          break;
-        case 'minLength':
-          validators.push(Validators.minLength(validator.value));
-          break;
-        case 'maxLength':
-          validators.push(Validators.maxLength(validator.value));
-          break;
-      }
+      //@ts-ignore
+      validators.push(DynamicFormValidators[validator.name](validator.value))
     });
     this.control = new FormControl(this.config?.value, validators);
     this.form.addControl(this.id, this.control);
