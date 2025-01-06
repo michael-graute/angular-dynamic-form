9def126b0029dc03ea4e734629b07f1e96f8ab63
Michael Graute <michael.graute@visitrans.de>
2025-01-06 17:43:48 +0100

    added prismjs

---
+++ src/app/helpers/prism/prism.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import Prism from 'prismjs'
+
+@Component({
+  selector: 'fg-prism',
+  imports: [],
+  templateUrl: './prism.component.html',
+  styleUrl: './prism.component.scss'
+})
+export class PrismComponent {
+
+}
